a8c9209dde7108233a4d96200f9d9199ba3181f6
Victor Gabardini <vgabardini@k12.com>
2023-09-01 12:24:17 -0300

    Added some changes

--- src/test/java/Runner/Runner.java
+++ src/test/java/Runner/Runner.java
@@ -1,11 +1,12 @@
-package Runner;
+package runner;
 
 import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources/Features",
+        features = "src/test/resources/features",
         glue = "Steps"
 )
 
